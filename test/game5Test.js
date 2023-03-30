67ae4e0d0260f53aa096d678fa52ea89b3a710fd
Braifz <braian94lp@gmail.com>
2023-03-30 15:33:40 -0300

    finish puzzle 5 :D

--- test/game5Test.js
+++ test/game5Test.js
@@ -7,14 +7,34 @@ describe("Game5", function () {
     const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
-    return { game, signer };
+    return { game };
   }
+
+  async function getAddress() {
+    const threshold = 0x00ffffffffffffffffffffffffffffffffffffff;
+    let address;
+    while (true) {
+      const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+      address = await wallet.getAddress();
+      if (address < threshold) {
+        return { address, wallet };
+      }
+    }
+  }
+
   it("should be a winner", async function () {
-    const { game, signer } = await loadFixture(deployContractAndSetVariables);
+    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { wallet, address } = await getAddress();
+
+    //the random address doesnt have gas, send some gas to it
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({
+      to: address,
+      value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+    });
 
     // good luck
-
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
