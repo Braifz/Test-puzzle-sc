68a3794565b192c329891f20d7b6f69226493116
Braifz <braian94lp@gmail.com>
2023-03-30 15:10:15 -0300

    add test 1,2,3,4,5

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,22 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
-    return { game };
+    return { game, signer };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, signer } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
 
     await game.win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
